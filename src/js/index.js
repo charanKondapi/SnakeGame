7d452942d3c26296e674349ce004726d8a2d2360
charan <charankondapi@gmail.com>
2020-01-01 02:29:31 +0530

    separated my bundler

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,31 @@
-import Board from './Board'
-import Snake from './Snake'
+import Board from "./Board";
+import Snake from "./Snake";
 
-const snakeBoard = new Board(500,500);
-      snakeBoard.drawBoard();
+const snakeBoard = new Board(500, 500);
+snakeBoard.drawBoard();
 
 const snake = new Snake();
-      snake.drawSnake();
+snake.drawSnake();
 
-snakeBoard.placeFood( snake.snakePosition );
-
+snakeBoard.placeFood(snake.snakePosition);
 
 // const timer = setInterval( () => snake.move( timer, snakeBoard, snake ), 200 );
 
-
-document.addEventListener('keydown',function( ev ){
-    setTimeout(()=>{
-        switch( ev.keyCode ){
-            case 37 :
-                Snake.DIRECTION = "left";
-                break;
-            case 38 : 
-                Snake.DIRECTION = "up";
-                break;
-            case 39 : 
-                Snake.DIRECTION = "right";
-                break;
-            case 40 : 
-                Snake.DIRECTION = "down";
-                break;
-        }
-    },0)
+document.addEventListener("keydown", function(ev) {
+  setTimeout(() => {
+    switch (ev.keyCode) {
+      case 37:
+        Snake.DIRECTION = "left";
+        break;
+      case 38:
+        Snake.DIRECTION = "up";
+        break;
+      case 39:
+        Snake.DIRECTION = "right";
+        break;
+      case 40:
+        Snake.DIRECTION = "down";
+        break;
+    }
+  }, 0);
 });
-
